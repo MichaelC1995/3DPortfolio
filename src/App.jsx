b8f1a7ef020d64617096328952103e0600ca348d
MichaelC1995 <m.chatellier@yahoo.com>
2025-07-27 21:46:01 -0500

    deployment fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import TechStack from "./sections/TechStack";
 import Hero from "./sections/Hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
 import FeatureCards from "./sections/FeatureCards";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
 import Footer from "./sections/Footer.jsx";
 
